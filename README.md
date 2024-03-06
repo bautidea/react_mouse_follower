dd81137d79cfa37fdd54b535888e883c1dcebdeb
bautidea <juanbautistadeangelis@gmail.com>
2024-03-06 10:59:11 -0300

    Project Finished.

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Mouse Follower
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+simple project to practice useEffect state, it consist of a mouse follower, wherever you move the cursor a ball will follow it.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Following effect can be toggled by a button, to enable or disable cursor following.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-  return <h3>New Project</h3>;
+  const [enable, setEnable] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove);
+    }
+
+    // Returning cleanup function.
+    // It will execute when the component dismount or when dependencies change
+    // before executing the effect again.
+    return () => {
+      window.removeEventListener('pointermove', handleMove);
+    };
+  }, [enable]);
+
+  return (
+    <main>
+      {enable && (
+        <div
+          className="pointer-follower"
+          style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+        />
+      )}
+
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? 'Disable' : 'Enable'} follow cursor
+      </button>
+    </main>
+  );
 }
 
 export default App;
